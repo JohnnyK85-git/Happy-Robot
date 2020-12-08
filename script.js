91bb02b61191229c8ad88926e4127c7a6aa5325f
JohnnyK85-git <johnnycbcs@gmail.com>
2020-12-08 15:56:12 +0100

    First Solving Attempt

---
+++ script.js
@@ -0,0 +1,9 @@
+let head = document.querySelector('#head');
+let rightEye = document.querySelector('#right-eye');
+let leftEye = document.querySelector('#left-eye');
+let mouth = document.querySelector('#mouth');
+let robotBody = document.querySelector('#robot-body');
+let arms = document.querySelector('#arms');
+let legs = document.querySelector('#legs');
+
+head.classList.add('square');
